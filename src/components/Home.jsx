362292307067aa7e9cf645b3bcebfa619a8fff71
keyy123 <parktwins1@gmail.com>
2023-02-01 19:18:05 -0500

    feedback: Too many issues addressed in one ticket will be concise with issue tickets

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = ({user, setUser}) => {
             <p>Welcome to project scrubz {user && user.account ? user?.account?.name : "Guest"}</p>
         </Layout>
         </div>
-  )
-}
+        )
+  }
 
 export default Home;
